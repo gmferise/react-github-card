b8f631e4ebc22664b3e6a21d992ea382d8b6d7b3
gmferise <46463513+gmferise@users.noreply.github.com>
2021-03-12 13:45:21 -0500

    Made github api portion more robust

--- src/apis/github.js
+++ src/apis/github.js
@@ -1,44 +1,58 @@
 import Debouncer from '../debouncer.js';
 
+const howLong = (timestamp) => {
+  const diff = timestamp - new Date().getTime();
+  const hours = Math.floor(diff / (1000 * 60 * 60));
+  const mins = Math.floor(diff / (1000 * 60)) % 60;
+  const secs = Math.floor(diff / (1000)) % 60;
+  return `${hours}hrs ${mins}min ${secs}s`;
+}
+
 const showRateLimit = (headers, endpoint) => {
-  console.log(`GitAPI Request Limit for ${endpoint}: ${headers.get('x-ratelimit-used')}/${headers.get('x-ratelimit-limit')} (Resets ${new Date(1000*headers.get('x-ratelimit-reset'))})`);
+  console.log(`${endpoint} limit: ${headers.get('x-ratelimit-used')}/${headers.get('x-ratelimit-limit')}\n(Resets in ${howLong(1000*headers.get('x-ratelimit-reset'))})`);
 };
 
 const getGithubUser = (username) => (
   username ? (
     fetch(`https://api.github.com/users/${username}`)
       .then((response) => {
-        console.log(response);
         showRateLimit(response.headers, '/users');
-        return response.json();
+        switch (response.status) {
+          case 200: return response.json();
+          case 403: return {
+            name: 'Failed to load profile',
+            login: 'Too many requests',
+            bio: 'Try again in '+howLong(1000*response.headers.get('x-ratelimit-reset')).split(' ').slice(1).join(' '),
+            avatar_url: 'https://via.placeholder.com/150',
+          };
+          default: return {
+            name: 'HTTP GET Error:',
+            login: response.status,
+            bio: 'Your request couldn\'t be handled, please try again later',
+            avatar_url: 'https://via.placeholder.com/150',
+          };
+        }
       })
   )
-  : ''
+  : {}
 );
-
-// const searchGithubUser = new Debouncer(
-//   500,
-//   (partialUsername) => (
-//     partialUsername ? (
-//       fetch(`https://api.github.com/search/users?q=${partialUsername}+type:user+in:login&per_page=5`)
-//         .then((response) => {
-//           console.log(`GitAPI Search Limit: ${response.headers.get('x-ratelimit-used')}/${response.headers.get('x-ratelimit-limit')} (Resets ${new Date(1000*response.headers.get('x-ratelimit-reset'))})`);
-//           return response.json().items.map((user) => user.login);
-//         })
-//     )
-//     : ''
-//   )
-// );
 
 const searchGithubUser = new Debouncer(
   500,
   (partialUsername) => (
     partialUsername ? (
-      new Promise((resolve, reject) => {
-        resolve([`You searched for "${partialUsername}"`]);
-      })
+      fetch(`https://api.github.com/search/users?q=${partialUsername}+type:user+in:login&per_page=5`)
+        .then((response) => {
+          showRateLimit(response.headers, '/search');
+          switch (response.status) {
+            case 200: return response.json();
+            case 403: return 'Failed to load suggestions, try again in '+howLong(1000*response.headers.get('x-ratelimit-reset')).split(' ')[2];
+            default: return `Error ${response.status}`;
+          }
+        })
+        .then((data) => data instanceof Object ? data.items.map((user) => user.login) : [data])
     )
-    : ['Nothing was searched...']
+    : []
   )
 );
 
